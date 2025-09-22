8f26cc57e8521cb4d67a469131db74c1636b4e1a
jacardenas <juanancardenas@gmail.com>
2025-09-22 16:00:21 +0200

    add Fraction #7

---
+++ src/main/java/es/upm/miw/devops/code/Fraction.java
@@ -0,0 +1,49 @@
+package es.upm.miw.devops.code;
+
+public class Fraction {
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        if (denominator == 0) {
+            throw new IllegalArgumentException("Denominator cannot be zero");
+        }
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public Fraction() {
+        this(1, 1);
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setDenominator(int denominator) {
+        if (denominator == 0) {
+            throw new IllegalArgumentException("Denominator cannot be zero");
+        }
+        this.denominator = denominator;
+    }
+
+    public double decimal() {
+        return (double) numerator / denominator;
+    }
+
+    @Override
+    public String toString() {
+        return "Fraction{" +
+                "numerator=" + numerator +
+                ", denominator=" + denominator +
+                '}';
+    }
+}
